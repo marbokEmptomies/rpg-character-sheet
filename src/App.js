4f1cc95a552081e603697066dac06c06161bf18d
Marko Soramäki <114909768+marbokEmptomies@users.noreply.github.com>
2023-10-31 13:19:42 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CharacterCreation from './components/CharacterCreation';
+
+function App() {
+  return (
+    <div className="App">
+      <CharacterCreation />
+    </div>
+  );
+}
+
+export default App;
